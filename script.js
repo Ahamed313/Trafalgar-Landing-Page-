f685c944c51a1c9f6adb3d4f19903b35d64b34dc
Ahamed313 <sabujkhagatua@gmail.com>
2026-02-06 12:12:06 +0800

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-const hamburger = document.getElementById("hamburger");
-const navLinks = document.getElementById("nav-links");
-
-hamburger.addEventListener("click", () => {
-  navLinks.style.display =
-    navLinks.style.display === "flex" ? "none" : "flex";
-});
